d11040e399811c8297adef0f28ed9770c89e87da
phish3y <logsdonevan1@gmail.com>
2022-10-19 22:24:50 -0700

    ecr push of image is working

--- cloudformation/roleoidc_create.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation create-stack --stack-name roleoidc --template-body file:///home/phish3y/dev/cicd/cloudformation/roleoidc.yaml --parameters ParameterKey=GitHubOrg,ParameterValue=phish3y ParameterKey=RepositoryName,ParameterValue=cicd --capabilities CAPABILITY_NAMED_IAM
